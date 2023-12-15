ebe5537f376e6bd42b5ae48ae557ce8bbcd49aa6
Dimon20012008 <peyton.b1329@yandex.ru>
2023-12-15 22:05:48 +0300

    Added storing previous and current position, tried drawing a line, but every new image overlaps it. Further work - flip image, so it is not mirror; draw lines on a new layer

--- main.py
+++ main.py
@@ -20,21 +20,31 @@ HEIGHT = img.get_height()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("pyGame Camera View")
 
+last_point = None
+current_point = None
 while True:
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             sys.exit()
     screen.blit(img, (0, 0))
-    pygame.display.flip()
-    img = webcam.get_image()
-
     view = pygame.surfarray.array3d(img).transpose([1, 0, 2])
 
     img_cv2 = cv2.cvtColor(view, cv2.COLOR_RGB2BGR)
     flipped = np.fliplr(img_cv2)
     flippedRGB = cv2.cvtColor(flipped, cv2.COLOR_BGR2RGB)
     results = handsDetector.process(flippedRGB)
-    if results.multi_hand_landmarks is None:
-        continue
-    finger = results.multi_hand_landmarks[0].landmark[8]
-    pygame.draw.circle(img, (0, 255, 0), ((1 - finger.x) * WIDTH, finger.y * HEIGHT), 3)
+    if results.multi_hand_landmarks is not None:
+        finger_tip_cords = results.multi_hand_landmarks[0].landmark[8]
+        if current_point is None:
+            last_point = ((1 - finger_tip_cords.x) * WIDTH, finger_tip_cords.y * HEIGHT)
+            current_point = ((1 - finger_tip_cords.x) * WIDTH, finger_tip_cords.y * HEIGHT)
+        else:
+            last_point = current_point
+            current_point = ((1 - finger_tip_cords.x) * WIDTH, finger_tip_cords.y * HEIGHT)
+            pygame.draw.line(screen, (0, 0, 0), last_point, current_point, width=1)
+    pygame.display.flip()
+    pygame.display.update()
+
+    img = webcam.get_image()
+
+
